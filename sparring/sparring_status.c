ef6df1589f11cc258d6c2e7c87a82a0d66558b5d
jed jarin <jedjarin@gmail.com>
2026-06-02 23:17:32 +0900

    updated

--- sparring/sparring_status.c
+++ sparring/sparring_status.c
@@ -3,24 +3,18 @@
 
 void printSparringStatus(Dog *player, Dog *enemy)
 {
-    printf("YOU: ");
+    printf("%-10s: ", player->name);
     printHPBar(player->hp, player->maxHP);
     printf(" [%d/%d]", player->hp, player->maxHP);
-
-    if (player->accDebuffTurns > 0)
-        printf(" (DAZED %d)", player->accDebuffTurns);
 
     if (player->isStunned && player->stunTurns > 0)
         printf(" (STUN %d)", player->stunTurns);
 
     printf("\n");
 
-    printf("ENEMY: ");
+    printf("%-10s: ", enemy->name);
     printHPBar(enemy->hp, enemy->maxHP);
     printf(" [%d/%d]", enemy->hp, enemy->maxHP);
-
-    if (enemy->accDebuffTurns > 0)
-        printf(" (DAZED %d)", enemy->accDebuffTurns);
 
     if (enemy->isStunned && enemy->stunTurns > 0)
         printf(" (STUN %d)", enemy->stunTurns);
