b72bcae842f51c5685efa821db767ee7b3758e7c
jed jarin <jedjarin@gmail.com>
2026-07-19 01:31:48 +0900

    credit.c

--- credit.c
+++ credit.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 #include "cinematic.h"
 #include "credit.h"
+#include "console.h"
+
+
+static void typeCenteredText(const char *text, int delay)
+{
+    int len = strlen(text);
+    int spaces = (CONSOLE_WIDTH - len) / 2;
+
+    if (spaces < 0)
+        spaces = 0;
+
+    for (int i = 0; i < spaces; i++)
+        printf(" ");
+
+    typeText(text, delay);
+    printf("\n");
+}
 
 void showCampaignEnding(void)
 {
     system("cls");
 
-    typeText("The Blacksite Laboratory has fallen.\n\n", 25);
-    typeText("The experiments have been stopped.\n", 25);
-    typeText("For now...\n\n", 25);
+    printBorder();
+    printBlankLine();
+    typeCenteredText("The Blacksite Laboratory has fallen.", 25);
+    printBlankLine();
+    typeCenteredText("The experiments have been stopped.", 25);
+    typeCenteredText("For now...", 25);
+    printBlankLine();
 
     waitForEnter();
 }
@@ -19,24 +41,27 @@ void showCredits(void)
 {
     system("cls");
 
-    typeText("========================================\n", 10);
-    typeText("               WARDOGZ\n", 10);
-    typeText("========================================\n\n", 10);
+    printBorder();
+    printBlankLine();
+    typeCenteredText("WARDOGZ", 10);
+    printBlankLine();
+    typeCenteredText("A C Console RPG Project", 15);
+    printBlankLine();
 
-    typeText("A C Console RPG Project\n\n", 15);
+    typeCenteredText("Created and Developed by", 15);
+    typeCenteredText("Jed Jarin", 15);
+    printBlankLine();
 
-    typeText("Created and Developed by\n\n", 15);
-    typeText("Jed Jarin\n\n", 15);
+    typeCenteredText("Special Thanks", 15);
+    typeCenteredText("CAP College", 15);
+    typeCenteredText("Family and Friends", 15);
+    printBlankLine();
 
-    typeText("Special Thanks\n\n", 15);
-
-    typeText("CAP College\n\n", 15);
-
-    typeText("Family and Friends\n\n", 15);
-
-    typeText("Thank you for playing.\n\n", 15);
-
-    typeText("CAMPAIGN COMPLETE\n", 15);
+    typeCenteredText("Thank you for playing.", 15);
+    printBlankLine();
+    typeCenteredText("CAMPAIGN COMPLETE", 15);
+    printBlankLine();
+    printBorder();
 
     waitForEnter();
 }
@@ -45,8 +70,12 @@ void showReplayEnding(void)
 {
     system("cls");
 
-    typeText("The Blacksite grows silent once more...\n\n", 25);
-    typeText("Campaign already completed.\n\n", 25);
+    printBorder();
+    printBlankLine();
+    typeCenteredText("The Blacksite grows silent once more...", 25);
+    printBlankLine();
+    typeCenteredText("Campaign already completed.", 25);
+    printBlankLine();
 
     waitForEnter();
 }
