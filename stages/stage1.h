e92c4bb154f7376288da28ddec830554bc24884d
jednorbie1993 <jedjarin@gmail.com>
2026-08-30 13:21:10 +0900

    stage 1 -5 .h remove

--- stages/stage1.h
+++
@@ -1,8 +0,0 @@
-#ifndef STAGE1_H
-#define STAGE1_H
-
-#include "../dog.h"
-
-void runStage1(Dog *player, int progress[]);
-
-#endif
--- stages/stage2.h
+++
@@ -1,8 +0,0 @@
-#ifndef STAGE2_H
-#define STAGE2_H
-
-#include "../dog.h"
-
-void runStage2(Dog *player, int progress[]);
-
-#endif
